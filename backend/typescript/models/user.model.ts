6becad89126d1ad77f6882f3d3aa214c2fa21781
christine <christineunlee@gmail.com>
2021-10-02 13:12:26 -0400

    Updated user.model.ts and types.ts

--- backend/typescript/models/user.model.ts
+++ backend/typescript/models/user.model.ts
@@ -3,6 +3,15 @@ import { Role } from "../types";
 
 @Table({ tableName: "users" })
 export default class User extends Model {
+  @Column({ type: DataType.INTEGER })
+  id!: number;
+
+  @Column({ type: DataType.STRING })
+  auth_id!: string;
+
+  @Column({ type: DataType.ENUM("Admin", "Member", "Author") })
+  role_type!: Role;
+
   @Column({ type: DataType.STRING })
   first_name!: string;
 
@@ -10,8 +19,8 @@ export default class User extends Model {
   last_name!: string;
 
   @Column({ type: DataType.STRING })
-  auth_id!: string;
+  email!: string;
 
-  @Column({ type: DataType.ENUM("User", "Admin") })
-  role!: Role;
+  @Column({ type: DataType.DATE })
+  created_at!: string;
 }
