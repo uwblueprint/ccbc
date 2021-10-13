c71b8e78c204303ad14a4d65ac08d4228081b310
Jessica Li <jessicabli0225@gmail.com>
2021-10-12 20:46:58 -0400

    added yml file and linted

--- frontend/src/components/auth/Logout.tsx
+++ frontend/src/components/auth/Logout.tsx
@@ -3,7 +3,6 @@ import React, { useContext } from "react";
 import authAPIClient from "../../APIClients/AuthAPIClient";
 import AuthContext from "../../contexts/AuthContext";
 
-
 const Logout = (): React.ReactElement => {
   const { authenticatedUser, setAuthenticatedUser } = useContext(AuthContext);
 
