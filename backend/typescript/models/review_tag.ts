0bfd8dd4de673a8209ccf17fdce05918b7c1f36e
Omar Baranek <obaranek@uwaterloo.ca>
2021-10-16 13:38:48 -0400

    added review_tag model

---
+++ backend/typescript/models/review_tag.ts
@@ -0,0 +1,15 @@
+import { Column, DataType, Model, Table, ForeginKey } from "sequelize-typescript";
+import Tag from "./tag.model"
+import Review from "./review.model"
+
+@Table({tableName: "review_tags"})
+export default class ReviewTag extends Model {
+
+    @ForeginKey(() => Review)
+    @Column({ type: DataType.INTEGER })
+    review_id!: number
+
+    @ForeginKey(() => Tag)
+    @Column({ type: DataType.INTEGER })
+    tag_id!: number
+}
