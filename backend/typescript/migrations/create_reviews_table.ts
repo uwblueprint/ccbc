18b276fb9a48cf6a6f923df6741755067ac674cc
Omar Baranek <obaranek@uwaterloo.ca>
2021-10-20 19:32:18 -0400

    create migrations for create reviews, tags, review_tag tables

---
+++ backend/typescript/migrations/create_reviews_table.ts
@@ -0,0 +1,45 @@
+import { DataType } from "sequelize-typescript"
+
+import { Migration } from "../umzug"
+
+export const up: Migration = async ({ context: sequelize }) => {
+    await sequelize.getQueryInterface().createTable("reviews", {
+        id:{
+            type: DataType.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        body: {
+            type:DataType.STRING,
+            allowNull: false,
+        },
+        cover_images: {
+            type: DataType.STRING,
+            allowNull: false,
+        },
+        byline: {
+            type: DataType.STRING,
+            allowNull: false,
+        },
+        featured: {
+            type: DataType.STRING,
+            allowNull: false,
+        },
+        published_at: {
+            type: DataType.DATE,
+            allowNull: false,
+        },
+        createdAt: {
+            allowNull: false,
+            type: DataType.DATE,
+        },
+        updatedAt: {
+            allowNull: false,
+            type: DataType.DATE,
+        },
+    });
+};
+
+export const down: Migration = async ({ context: sequelize }) => {
+    await sequelize.getQueryInterface().dropTable("reviews");
+}
