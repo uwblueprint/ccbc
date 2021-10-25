d923523f04a4e55d8dc350ecf8df5797a3dcac3e
Omar Baranek <59672562+obaranek@users.noreply.github.com>
2021-10-24 20:34:56 -0400

    Create Reviews and Tags models (#7)
    Co-authored-by: christine <christineunlee@gmail.com>
    Co-authored-by: Tahmeed27 <Tahmeed27@users.noreply.github.com>

---
+++ backend/typescript/models/review_tag.model.ts
@@ -0,0 +1,22 @@
+/* eslint import/no-cycle: 0 */
+
+import {
+  Column,
+  DataType,
+  Model,
+  Table,
+  ForeignKey,
+} from "sequelize-typescript";
+import Tag from "./tag.model";
+import Review from "./review.model";
+
+@Table({ tableName: "review_tag" })
+export default class ReviewTag extends Model {
+  @ForeignKey(() => Review)
+  @Column({ type: DataType.INTEGER })
+  review_id!: number;
+
+  @ForeignKey(() => Tag)
+  @Column({ type: DataType.INTEGER })
+  tag_id!: number;
+}
