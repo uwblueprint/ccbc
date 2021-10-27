c50637dcee790130e0d46a8a2131747ffaaa3a65
Tayef Shah <tayefshah@gmail.com>
2021-10-27 19:59:40 -0400

    add firebase service account to google credentials

--- backend/typescript/nodemailer.config.ts
+++ backend/typescript/nodemailer.config.ts
@@ -1,6 +1,6 @@
 import googleCredentials from "./google-credentials.json";
 import { NodemailerConfig } from "./types";
 
-const config: NodemailerConfig = googleCredentials;
+const config: NodemailerConfig = googleCredentials.nodemailerConfig;
 
 export default config;
