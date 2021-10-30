059f05238039c0b98a92d5f87b821c1ad44011f9
Tayef Shah <tayefshah@gmail.com>
2021-10-30 14:08:20 -0400

    add sequelize options to testdb

--- backend/typescript/testUtils/testDb.ts
+++ backend/typescript/testUtils/testDb.ts
@@ -1,14 +1,26 @@
 import { resolve } from "path";
 
-import { Sequelize } from "sequelize-typescript";
+import { Sequelize, SequelizeOptions } from "sequelize-typescript";
 
 const dbURL =
   process.env.DATABASE_URL ||
   `postgres://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.DB_HOST}:5432/${process.env.POSTGRES_DB}`;
 
-const testSql = new Sequelize(dbURL, {
-  models: [resolve(__dirname, "../models/*.model.ts")],
-  logging: false,
-});
+const sequelizeOptions: SequelizeOptions = process.env.DATABASE_URL
+  ? {
+      dialect: "postgres",
+      protocol: "postgres",
+      dialectOptions: {
+        ssl: true,
+      },
+      models: [resolve(__dirname, "../models/*.model.ts")],
+      logging: false,
+    }
+  : {
+      models: [resolve(__dirname, "../models/*.model.ts")],
+      logging: false,
+    };
+
+const testSql = new Sequelize(dbURL, sequelizeOptions);
 
 export default testSql;
