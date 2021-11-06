c514ee7b75b315cd2f6355d5928a41b596c26ca6
christine <christineunlee@gmail.com>
2021-11-06 13:31:59 -0400

    Added custom fonts to theme

--- frontend/src/components/auth/Login.tsx
+++ frontend/src/components/auth/Login.tsx
@@ -1,3 +1,15 @@
+import {
+  Box,
+  Button,
+  FormControl,
+  FormHelperText,
+  FormLabel,
+  Grid,
+  GridItem,
+  Input,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
 import React, { useContext, useState } from "react";
 import {
   GoogleLogin,
@@ -5,8 +17,6 @@ import {
   GoogleLoginResponseOffline,
 } from "react-google-login";
 import { Redirect, useHistory } from "react-router-dom";
-
-import { Box, Grid, Text, GridItem, Stack, FormHelperText, FormErrorMessage, Link, FormControl, Button, Input, FormLabel, Heading } from "@chakra-ui/react";
 
 import authAPIClient from "../../APIClients/AuthAPIClient";
 import { HOME_PAGE, SIGNUP_PAGE } from "../../constants/Routes";
@@ -17,7 +27,7 @@ type GoogleResponse = GoogleLoginResponse | GoogleLoginResponseOffline;
 
 const Login = (): React.ReactElement => {
   const { authenticatedUser, setAuthenticatedUser } = useContext(AuthContext);
-  const [show, setShow] = useState(false)
+  const [show, setShow] = useState(false);
   const [isInvalid, setInvalid] = useState(false);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -37,15 +47,17 @@ const Login = (): React.ReactElement => {
 
   return (
     <Grid templateColumns="repeat(2, 1fr)">
-      <GridItem w="100%" h="100%" bg="papayawhip"/>
+      <GridItem w="100%" h="100%" bg="papayawhip" />
       <GridItem>
         <Stack justify="center" p="20%">
-          <Heading as="h1">Log in</Heading>
-          <Text color="gray.700">Enter your credentials to access your account.</Text>
+          <Text textStyle="heading">Log in</Text>
+          <Text textStyle="body" color="gray.700">
+            Enter your credentials to access your account.
+          </Text>
           <FormControl mt="1rem">
-            <Box mt="4%"> 
+            <Box mt="4%">
               <FormLabel>Email address</FormLabel>
-              <Input 
+              <Input
                 value={email}
                 name="email"
                 placeholder="Email address"
@@ -54,14 +66,14 @@ const Login = (): React.ReactElement => {
             </Box>
             <Box mt="4%" mb="10%">
               <FormLabel>Password</FormLabel>
-              <Input 
+              <Input
                 isInvalid={isInvalid}
                 value={password}
                 type="password"
                 name="password"
                 placeholder="Password"
                 onChange={(event) => {
-                  setPassword(event.target.value)
+                  setPassword(event.target.value);
                   setInvalid(false);
                 }}
                 errorBorderColor="crimson"
--- frontend/src/theme/index.tsx
+++ frontend/src/theme/index.tsx
@@ -1,6 +1,19 @@
+import "@fontsource/open-sans";
+import "@fontsource/coustard";
+
 import { extendTheme } from "@chakra-ui/react";
 
 const customTheme = extendTheme({
+  textStyles: {
+    heading: {
+      fontFamily: "Coustard",
+      fontSize: "30px",
+    },
+    body: {
+      fontFamily: "Open Sans",
+      fontSize: "15px"
+    },
+  },
   config: {
     initialColorMode: "light",
   },
