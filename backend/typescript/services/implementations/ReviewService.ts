9d493ff01884d0e479996493916c9a4d21bf2220
Omar Baranek <obaranek@uwaterloo.ca>
2021-11-10 21:28:16 -0500

    added createReview service

--- backend/typescript/services/implementations/ReviewService.ts
+++ backend/typescript/services/implementations/ReviewService.ts
@@ -1,14 +1,19 @@
 import { resolve } from "path";
-import sequelize from "sequelize";
 import { Sequelize } from "sequelize-typescript";
 import PgReview from "../../models/review.model";
+import PgBook from "../../models/book.model";
 import PgTag from "../../models/tag.model";
-import PgReviewTag from "../../models/review_tag.model";
+import PgSeries from "../../models/series.model";
+import PgAuthor from "../../models/author.model";
+import PgPublisher from "../../models/publisher.model";
 import logger from "../../utilities/logger";
 import {
   ReviewRequestDTO,
   IReviewService,
   ReviewResponseDTO,
+  Book,
+  Author,
+  Publisher,
 } from "../interfaces/IReviewService";
 
 const Logger = logger(__filename);
@@ -27,11 +32,14 @@ class ReviewService implements IReviewService {
   }
 
   /* eslint-disable class-methods-use-this */
-  async createEntity(review: ReviewRequestDTO): Promise<ReviewResponseDTO> {
+  async createReview(review: ReviewRequestDTO): Promise<ReviewResponseDTO> {
     let newReview: PgReview;
+    let newBook: PgBook;
     let tag: PgTag;
-    let tagCreated: boolean;
-    let result;
+    let series: PgSeries;
+    let author: PgAuthor;
+    let publisher: PgPublisher;
+    let result: ReviewResponseDTO;
 
     try {
       result = await this.db.transaction(async (t) => {
@@ -41,32 +49,114 @@ class ReviewService implements IReviewService {
             cover_images: review.coverImages,
             byline: review.byline,
             featured: review.featured,
-            published_at: review.publishedAt,
+            published_at: review.publishedAt || null,
           },
           { transaction: t },
         );
 
-        await Promise.all(
+        const tagsRet: PgTag[] = await Promise.all(
           review.tags.map(async (reviewTag) => {
-            [tag, tagCreated] = await PgTag.findOrCreate({
+            tag = await PgTag.findOrCreate({
               where: { name: reviewTag.name },
               transaction: t,
-            });
-
+            }).then((data) => data[0]);
             await newReview.$add("tags", tag, { transaction: t });
+            return tag;
           }),
         );
 
-        return newReview;
+        const booksRet: Book[] = await Promise.all(
+          review.books.map(async (book: Book) => {
+            series = await PgSeries.findOrCreate({
+              where: { name: book.seriesName },
+              transaction: t,
+            }).then((data) => data[0]);
+
+            newBook = await PgBook.create(
+              {
+                review_id: newReview.id,
+                title: book.title,
+                series_id: series.id,
+                series_order: book.seriesOrder || null,
+                illustrator: book.illustrator || null,
+                translator: book.translator || null,
+                formats: book.formats,
+                age_range: [book.minAge, book.maxAge],
+              },
+              { transaction: t },
+            );
+
+            const authorsRet: Author[] = await Promise.all(
+              book.authors.map(async (a) => {
+                author = await PgAuthor.findOrCreate({
+                  where: {
+                    full_name: a.fullName,
+                    display_name: a.displayName || null,
+                    attribution: a.attribution || null,
+                  },
+                  transaction: t,
+                }).then((data) => data[0]);
+                newBook.$add("authors", author, { transaction: t });
+                return {
+                  fullName: author.full_name,
+                  displayName: author.display_name,
+                  attribution: author.attribution,
+                };
+              }),
+            );
+
+            const publishersRet: Publisher[] = await Promise.all(
+              book.publishers.map(async (p) => {
+                publisher = await PgPublisher.findOrCreate({
+                  where: {
+                    full_name: p.fullName,
+                    publish_year: p.publishYear,
+                  },
+                  transaction: t,
+                }).then((data) => data[0]);
+                return {
+                  fullName: publisher.full_name,
+                  publishYear: publisher.publish_year,
+                };
+              }),
+            );
+
+            await newBook.$add("publishers", publisher, { transaction: t });
+            await newReview.$add("books", newBook, { transaction: t });
+            return {
+              title: newBook.title,
+              seriesOrder: newBook.series_order,
+              illustrator: newBook.illustrator,
+              translator: newBook.translator,
+              formats: newBook.formats,
+              // TODO: change hard-coded values
+              minAge: newBook.age_range[0].value,
+              maxAge: newBook.age_range[1].value,
+              authors: authorsRet,
+              publishers: publishersRet,
+              seriesName: series.name,
+            };
+          }),
+        );
+
+        return {
+          reviewId: newReview.id,
+          body: newReview.body,
+          cover_images: newReview.cover_images,
+          byline: newReview.byline,
+          featured: newReview.featured,
+          books: booksRet,
+          tags: tagsRet,
+          updatedAt: newReview.updatedAt,
+          publishedAt: newReview.published_at,
+        };
       });
     } catch (error) {
       Logger.error(`Failed to create entity. Reason = ${error.message}`);
       throw error;
     }
 
-    return {
-      reviewId: result.id,
-    };
+    return result;
   }
 }
 
