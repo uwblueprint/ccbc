6a3c989d3fa3c49e23b234b3b553057293dbf036
r-aya <rayajaima@gmail.com>
2021-11-20 12:30:51 -0500

    Fixed Issues from Lint Check
    Added react-icons to dependencies, added alt prop to CCBC logo; other minor formatting issues fixed.

--- frontend/src/components/common/NavBar.tsx
+++ frontend/src/components/common/NavBar.tsx
@@ -1,44 +1,54 @@
 import React, { useContext } from "react";
-import logo from "../../ccbc.png";
-import {Link, useLocation} from "react-router-dom";
 import { HiUser } from "react-icons/hi";
-import { Route, Redirect } from "react-router-dom";
-import { UserRole } from "../../constants/Enums";
+import { Link } from "react-router-dom";
 
+import logo from "../../ccbc.png";
+import { UserRole } from "../../constants/Enums";
 import AuthContext from "../../contexts/AuthContext";
 
 const NavBar = (): React.ReactElement => {
-    const { authenticatedUser } = useContext(AuthContext);
-    const isAdmin = authenticatedUser?.role == UserRole.Admin;
+  const { authenticatedUser } = useContext(AuthContext);
+  const isAdmin = authenticatedUser?.role === UserRole.Admin;
 
-    return (
-                <div style={{
-                    display: "flex", 
-                    flexDirection: "row", 
-                    height: "calc(96px - 32px)",
-                    width: "calc(100vw - 160px)",
-                    backgroundColor: "#2D5577", 
-                    alignItems: "center",
-                    padding: "16px 80px",
-                    position: "absolute",
-                    justifyContent: "space-between",
-                    top: 0}}>
-                        
-                    <div style={{display: "flex", flexDirection: "row", alignItems: "center"}}>
-                        <Link to="/app/home"><img src={logo} style={{height: "60px", borderRadius: "50px", marginRight: "30px"}}></img></Link>
-                        <h4 style={{color: "#fff", margin: "30px"}}>Home</h4>
-                        {isAdmin && 
-                            <h4 style={{color: "#fff", margin: "30px"}}>Admin Dashboard</h4>
-                        }
-                        
-                    </div>
-                    <div>
-                        
-                        <HiUser style={{color: "#fff", height: "40px", width: "40px"}}></HiUser>
-                    </div>
-                </div>
-            )
-        }
-        
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        height: "calc(96px - 32px)",
+        width: "calc(100vw - 160px)",
+        backgroundColor: "#2D5577",
+        alignItems: "center",
+        padding: "16px 80px",
+        position: "absolute",
+        justifyContent: "space-between",
+        top: 0,
+      }}
+    >
+      <div
+        style={{ display: "flex", flexDirection: "row", alignItems: "center" }}
+      >
+        <Link to="/app/home">
+          <img
+            src={logo}
+            alt="CCBC Logo"
+            style={{
+              height: "60px",
+              borderRadius: "50px",
+              marginRight: "30px",
+            }}
+          />
+        </Link>
+        <h4 style={{ color: "#fff", margin: "30px" }}>Home</h4>
+        {isAdmin && (
+          <h4 style={{ color: "#fff", margin: "30px" }}>Admin Dashboard</h4>
+        )}
+      </div>
+      <div>
+        <HiUser style={{ color: "#fff", height: "40px", width: "40px" }} />
+      </div>
+    </div>
+  );
+};
+
 export default NavBar;
-
