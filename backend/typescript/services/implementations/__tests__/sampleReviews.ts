970011d788d4b25a36d2f60e2420be27408d2fe1
Omar Baranek <obaranek@uwaterloo.ca>
2021-11-25 21:42:35 -0500

    added sample reviews

---
+++ backend/typescript/services/implementations/__tests__/sampleReviews.ts
@@ -0,0 +1,61 @@
+export default [
+  {
+    body: "review100",
+    coverImages: ["cover1", "cover2"],
+    byline: "byline1",
+    featured: true,
+    publishedAt: 12445,
+    books: [
+      {
+        title: "title1",
+        titlePrefix: "titlePrefix1",
+        seriesOrder: "1",
+        illustrator: ["illustrator1"],
+        translator: ["translator1"],
+        formats: [
+          {
+            format: "format1",
+            price: "$15.0",
+            isbn: "1234-5678-931",
+          },
+        ],
+        minAge: 3,
+        maxAge: 18,
+        authors: [{ fullName: "author11" }],
+        publishers: [
+          {
+            fullName: "publisher",
+            publishYear: 2002,
+          },
+        ],
+        seriesName: "seriesName",
+      },
+      {
+        title: "title2",
+        titlePrefix: "titlePrefix1",
+        seriesOrder: "1",
+        illustrator: ["illustrator1"],
+        translator: ["translator1"],
+        formats: [
+          {
+            format: "format1",
+            price: "$15.0",
+            isbn: "1234-5678-931",
+          },
+        ],
+        minAge: 3,
+        maxAge: 18,
+        authors: [{ fullName: "author11" }],
+        publishers: [
+          {
+            fullName: "publisher",
+            publishYear: 2002,
+          },
+        ],
+        seriesName: "seriesName",
+      },
+    ],
+    tags: [{ name: "tag1" }, { name: "tag2" }],
+    createdBy: 1,
+  },
+];
