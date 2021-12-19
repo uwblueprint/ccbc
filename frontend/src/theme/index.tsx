8cec7571681ae3b71d3c2397589c92400add7181
r-aya <32601085+r-aya@users.noreply.github.com>
2021-12-19 14:35:06 -0500

    Add Navigation Bar (#23)
    * Navigation Bar
    
    Created navigation bar and uploaded CCBC logo png
    
    * Fixed Issues from Lint Check
    
    Added react-icons to dependencies, added alt prop to CCBC logo; other minor formatting issues fixed.
    
    * Delete package-lock.json
    
    * Implemented Chakra
    
    Created new text style and container style with chakra for navigation bar
    
    * Fixed syntax errors involving Chakra
    
    Fixed syntax errors, converted everything to Chakra components, added base customizations to h4 tag
    
    * Changed roletype
    
    * fixed error:unknown errors in EntityAPIClient
    
    * Added profile dropdown
    
    * added navbar to home page
    
    * fixed navbar in home page
    
    * Profile Dropdown added
    
    Added profile dropdown with a functioning 'sign out' button; changed NavBar background color
    
    * NavBar size changes + theme font update
    
    Changed size of navbar and profile dropdown fonts; updated fonts in theme file to DM Sans
    
    * Add Navbar to private route
    
    * Removed comment
    
    Co-authored-by: Tahmeed27 <51849738+Tahmeed27@users.noreply.github.com>
    Co-authored-by: Dhruv Mittal <d2mittal@edu.uwaterloo.ca>
    Co-authored-by: Tahmeed Mostafiz <tmostafiz@gmail.com>

--- frontend/src/theme/index.tsx
+++ frontend/src/theme/index.tsx
@@ -1,5 +1,6 @@
 import "@fontsource/open-sans";
 import "@fontsource/coustard";
+import "@fontsource/dm-sans";
 
 import { extendTheme } from "@chakra-ui/react";
 
@@ -10,8 +11,15 @@ const customTheme = extendTheme({
       fontSize: "2vw",
     },
     body: {
-      fontFamily: "Open Sans",
-      fontSize: "1vw",
+      fontFamily: "DM Sans",
+      fontSize: "16px",
+    },
+    h4: {
+      fontFamily: "DM Sans",
+      color: "#fff",
+      margin: "30px",
+      fontSize: "md",
+      fontWeight: "bold",
     },
   },
   components: {
@@ -22,6 +30,14 @@ const customTheme = extendTheme({
           bg: "teal.500",
           color: "white",
           size: "md",
+        },
+      },
+    },
+    Heading: {
+      variants: {
+        nav: {
+          color: "#fff",
+          margin: "30px",
         },
       },
     },
