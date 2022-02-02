f423fe2d4488d3462e138898e746161d50f0f201
Alex Yu <alexjy@yahoo.com>
2022-02-02 17:11:29 -0500

    add role auth middleware to review and tag service endpoints (#40)

--- backend/typescript/rest/reviewRoutes.ts
+++ backend/typescript/rest/reviewRoutes.ts
@@ -9,70 +9,88 @@ import {
 import { getErrorMessage, sendErrorResponse } from "../utilities/errorResponse";
 import sendResponseByMimeType from "../utilities/responseUtil";
 import reviewRequestDtoValidator from "../middlewares/validators/reviewValidators";
+import { isAuthorizedByRole } from "../middlewares/auth";
 
 const reviewRouter: Router = Router();
 const reviewService: IReviewService = new ReviewService();
 
-reviewRouter.post("/", reviewRequestDtoValidator, async (req, res) => {
-  const contentType = req.headers["content-type"];
-  try {
-    const newReview = await reviewService.createReview({
-      body: req.body.body,
-      byline: req.body.byline,
-      featured: req.body.featured,
-      createdBy: req.body.createdBy,
-      books: req.body.books as BookRequest[],
-      tags: req.body.tags as Tag[],
-      publishedAt: req.body.publishedAt,
-    });
-    await sendResponseByMimeType<ReviewResponseDTO>(
-      res,
-      200,
-      contentType,
-      newReview,
-    );
-  } catch (e: unknown) {
-    sendErrorResponse(e, res);
-  }
-});
+reviewRouter.post(
+  "/",
+  isAuthorizedByRole(new Set(["Admin"])),
+  reviewRequestDtoValidator,
+  async (req, res) => {
+    const contentType = req.headers["content-type"];
+    try {
+      const newReview = await reviewService.createReview({
+        body: req.body.body,
+        byline: req.body.byline,
+        featured: req.body.featured,
+        createdBy: req.body.createdBy,
+        books: req.body.books as BookRequest[],
+        tags: req.body.tags as Tag[],
+        publishedAt: req.body.publishedAt,
+      });
+      await sendResponseByMimeType<ReviewResponseDTO>(
+        res,
+        200,
+        contentType,
+        newReview,
+      );
+    } catch (e: unknown) {
+      sendErrorResponse(e, res);
+    }
+  },
+);
 
-reviewRouter.get("/:id", async (req, res) => {
-  const { id } = req.params;
-  try {
-    const review = await reviewService.getReview(id);
-    res.status(200).json(review);
-  } catch (e: unknown) {
-    sendErrorResponse(e, res);
-  }
-});
+reviewRouter.get(
+  "/:id",
+  isAuthorizedByRole(new Set(["Admin", "Subscriber", "Author"])),
+  async (req, res) => {
+    const { id } = req.params;
+    try {
+      const review = await reviewService.getReview(id);
+      res.status(200).json(review);
+    } catch (e: unknown) {
+      sendErrorResponse(e, res);
+    }
+  },
+);
 
-reviewRouter.get("/", async (req, res) => {
-  const contentType = req.headers["content-type"];
-  try {
-    const reviews = await reviewService.getReviews();
-    await sendResponseByMimeType<ReviewResponseDTO[]>(
-      res,
-      200,
-      contentType,
-      reviews,
-    );
-  } catch (e: unknown) {
-    await sendResponseByMimeType(res, 500, contentType, [
-      {
-        error: getErrorMessage(e),
-      },
-    ]);
-  }
-});
+reviewRouter.get(
+  "/",
+  isAuthorizedByRole(new Set(["Admin", "Subscriber", "Author"])),
+  async (req, res) => {
+    const contentType = req.headers["content-type"];
+    try {
+      const reviews = await reviewService.getReviews();
+      await sendResponseByMimeType<ReviewResponseDTO[]>(
+        res,
+        200,
+        contentType,
+        reviews,
+      );
+    } catch (e: unknown) {
+      await sendResponseByMimeType(res, 500, contentType, [
+        {
+          error: getErrorMessage(e),
+        },
+      ]);
+    }
+  },
+);
 
-reviewRouter.delete("/:id", async (req, res) => {
-  const { id } = req.params;
-  try {
-    await reviewService.deleteReview(id);
-    res.status(204).send();
-  } catch (e: unknown) {
-    sendErrorResponse(e, res);
-  }
-});
+reviewRouter.delete(
+  "/:id",
+  isAuthorizedByRole(new Set(["Admin"])),
+  async (req, res) => {
+    const { id } = req.params;
+    try {
+      await reviewService.deleteReview(id);
+      res.status(204).send();
+    } catch (e: unknown) {
+      sendErrorResponse(e, res);
+    }
+  },
+);
 
 export default reviewRouter;
