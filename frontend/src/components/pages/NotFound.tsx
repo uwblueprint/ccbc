6439ee2ab03ddea8525a30665a254932be90e49b
r-aya <rayajaima@gmail.com>
2022-02-06 17:22:18 -0500

    Changes to Background Styling

--- frontend/src/components/pages/NotFound.tsx
+++ frontend/src/components/pages/NotFound.tsx
@@ -1,16 +1,13 @@
-import { Flex, Image, Text } from "@chakra-ui/react";
+import { Flex, Image, Text} from "@chakra-ui/react";
 import React from "react";
 import { Link } from "react-router-dom";
 
 import logo from "../../assets/ccbc.png";
-import ellipse from "../../assets/ellipse.svg";
-import ellipse2 from "../../assets/ellipse2.svg";
 import background from "../../assets/NotFoundBack.png";
-import polygon from "../../assets/polygon.svg";
 import character from "../../assets/sitting.svg";
-import star from "../../assets/star.svg";
 
 const NotFound = (): React.ReactElement => {
+
   return (
     <Flex
       direction="column"
@@ -18,8 +15,12 @@ const NotFound = (): React.ReactElement => {
       w="100vw"
       justify="center"
       align="center"
-      // bgColor="#000000"
-      backgroundImage={background}
+      bgImage={background}
+      bgPosition="center"
+      bgRepeat="no-repeat"
+      backgroundAttachment="scroll"
+      overflowY="scroll"
+      padding="50px 30px"
     >
       <Link to="/">
         <Image
@@ -33,15 +34,15 @@ const NotFound = (): React.ReactElement => {
           boxShadow="base"
         />
       </Link>
+
       <Text
         textStyle="heading"
         fontSize="42px"
         fontWeight="extrabold"
-        marginTop="40px"
-        zIndex="1"
       >
         Oops! Something went wrong!
       </Text>
+
       <Text textStyle="body" marginTop="10px" marginBottom="70px" zIndex="1">
         Please refresh the page and try again.
       </Text>
@@ -52,43 +53,7 @@ const NotFound = (): React.ReactElement => {
         boxSize="350px"
         zIndex="1"
       />
-      {/* <Image
-        src={star}
-        alt="orange star"
-        boxSize="270px"
-        bottom="-40px"
-        left="60px"
-        position="fixed"
-        zIndex="0"
-      /> */}
-      {/* 
-      <Image
-        src={ellipse}
-        alt="red half circle"
-        boxSize="230px"
-        position="fixed"
-        zIndex="0"
-        right="-10px"
-        bottom="60px"
-      />
-      <Image
-        src={ellipse2}
-        alt="yellow half circle"
-        boxSize="200px"
-        position="fixed"
-        zIndex="0"
-        right="-10px"
-        top="80px"
-      />
-      <Image
-        src={polygon}
-        alt="light blue triangle"
-        boxSize="200px"
-        position="fixed"
-        zIndex="0"
-        left="260px"
-        top="120px"
-      /> */}
+      
     </Flex>
   );
 };
