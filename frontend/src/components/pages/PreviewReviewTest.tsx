3d9e9a57db155ab959c08ce3054dbfbfaa59d05c
Mahad Zaryab <43658574+mahadzaryab1@users.noreply.github.com>
2022-02-28 11:19:47 -0500

    Create Component For Previewing Reviews (#48)
    * Add New Route For Testing Preview Review Component
    
    * Create Preview Review Component
    
    * Render Component Using Test Data
    
    * Add Documentation For Props And Component
    
    * Add TODO Comments For Removing Test Pages
    
    * Move Font Style To Top Component
    
    * Remove Pixel Sizing
    
    * Fix Comment
    
    * Move Modal Inside Component
    
    * Address Feedback From PR Review
    
    * Address Feedback From Design Review
    
    * Add Border Radius To Book Cover Image
    
    * Make Text Size Bigger And Center Modal
    
    * Adjust Font Sizes For Modal Header
    
    * Refactor To Use Seperate Component For Review
    
    * Adjust Centering For Modal Header
    
    * Add Border For Modal Header

---
+++ frontend/src/components/pages/PreviewReviewTest.tsx
@@ -0,0 +1,39 @@
+import { Button, useDisclosure } from "@chakra-ui/react";
+import React from "react";
+
+import PreviewReviewModal from "../PreviewReviewModal";
+
+/**
+ * A test page for testing the PreviewReviewModal
+ * TODO: Remove once PreviewReviewModal is integrated
+ */
+const PreviewReviewTest = (): React.ReactElement => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  return (
+    <>
+      <Button onClick={onOpen}>Open Modal</Button>
+      <PreviewReviewModal
+        title="Cat in the Hat"
+        subtitle="Adventures of the Cat in the Hat"
+        writtenBy="Sophia Spencer and Pencer Spencer"
+        reviewedBy="Border Porterson"
+        publisher="Publisher Fitzsherbur, 2014"
+        isbn="Soft cover, 978-3-16-148410-0"
+        bookType="Picture book"
+        ageDesciption="Ages 3-6"
+        body="<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+        do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco
+        laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+        irure dolor in reprehenderit in voluptate velit esse cillum
+        dolore eu fugiat nulla pariatur.</p>"
+        tags={["Cheerful", "Pain", "Cats"]}
+        coverUrl="https://upload.wikimedia.org/wikipedia/en/1/10/The_Cat_in_the_Hat.png"
+        isOpen={isOpen}
+        onClose={onClose}
+      />
+    </>
+  );
+};
+
+export default PreviewReviewTest;
