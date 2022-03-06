f4c81d1558d3b4121a126ba4358c238757dd6ea4
Mahad Zaryab <mahadzaryab1@gmail.com>
2022-03-06 11:07:44 -0500

    Add Documentation For New Pages

--- frontend/src/components/pages/CreateReviewPage.tsx
+++ frontend/src/components/pages/CreateReviewPage.tsx
@@ -2,6 +2,9 @@ import React from "react";
 
 import CreateReview from "./CreateReview/CreateReview";
 
+/**
+ * This page is used to create a new Review
+ */
 const CreateReviewPage = (): React.ReactElement => {
   return <CreateReview />;
 };
