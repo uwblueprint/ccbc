bcf214db6da57388624bb3a858806888fc89ce06
Mahad Zaryab <mahadzaryab1@gmail.com>
2022-03-17 17:05:07 -0400

    Remove Existing Migrations And Add Test Migration

--- backend/typescript/migrations/20220317210403-test-migration.ts
+++ backend/typescript/migrations/20220317210403-test-migration.ts
@@ -3,19 +3,15 @@ import { DataType } from "sequelize-typescript";
 import { Migration } from "../umzug";
 
 export const up: Migration = async ({ context: sequelize }) => {
-  await sequelize.getQueryInterface().createTable("tags", {
+  await sequelize.getQueryInterface().createTable("test", {
     id: {
       type: DataType.INTEGER,
       primaryKey: true,
       autoIncrement: true,
     },
-    name: {
-      type: DataType.STRING,
-      allowNull: false,
-    },
   });
 };
 
 export const down: Migration = async ({ context: sequelize }) => {
-  await sequelize.getQueryInterface().dropTable("tags");
+  await sequelize.getQueryInterface().dropTable("test");
 };
