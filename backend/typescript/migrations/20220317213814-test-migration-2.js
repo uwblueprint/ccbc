8f4b5021d25eaeebc92df41c6cc988e60fdc9b2f
Mahad Zaryab <mahadzaryab1@gmail.com>
2022-03-17 17:39:32 -0400

    Try JS Migration Instead of TS

---
+++ backend/typescript/migrations/20220317213814-test-migration-2.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('test', {
+      name: Sequelize.DataTypes.STRING,
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('test');
+  }
+};
+
