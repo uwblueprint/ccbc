fe0a2234dafd7d4512ccfbb80f9ca4399bcd5eb8
r-aya <rayajaima@gmail.com>
2022-03-16 20:06:13 -0400

    Fixed Linting Issues

--- frontend/src/components/pages/SearchBox.tsx
+++ frontend/src/components/pages/SearchBox.tsx
@@ -1,26 +1,39 @@
-import { Search2Icon } from '@chakra-ui/icons';
-import { Input, InputGroup, InputLeftElement, InputRightAddon, InputRightElement } from "@chakra-ui/react";
+import { Search2Icon } from "@chakra-ui/icons";
+import {
+  Input,
+  InputGroup,
+  InputLeftElement,
+  InputRightElement,
+} from "@chakra-ui/react";
 import React from "react";
 
 const SearchBox = (): React.ReactElement => {
   return (
     <div style={{ textAlign: "center", padding: "10px 20px" }}>
-        <InputGroup borderColor= "#110A23">
-                <InputLeftElement
-                    pointerEvents='none'
-                    color="#110A23"
-                    
-                >
-                    <Search2Icon/>
-                </InputLeftElement>
-            <InputRightElement h="full" w="auto" bg="#110A23" color="white" padding="0 26px" fontSize='14px' textStyle="body" borderRadius="0 8px 8px 0">Search</InputRightElement>
-            <Input 
-                focusBorderColor="#110A23" 
-                _hover = {{ borderColor:"#110A23"}}
-                fontSize="14px" 
-                placeholder='Search' 
-                textStyle="body"/>
-        </InputGroup>
+      <InputGroup borderColor="#110A23">
+        <InputLeftElement pointerEvents="none" color="#110A23">
+          <Search2Icon />
+        </InputLeftElement>
+        <InputRightElement
+          h="full"
+          w="auto"
+          bg="#110A23"
+          color="white"
+          padding="0 26px"
+          fontSize="14px"
+          textStyle="body"
+          borderRadius="0 8px 8px 0"
+        >
+          Search
+        </InputRightElement>
+        <Input
+          focusBorderColor="#110A23"
+          _hover={{ borderColor: "#110A23" }}
+          fontSize="14px"
+          placeholder="Search"
+          textStyle="body"
+        />
+      </InputGroup>
     </div>
   );
 };
