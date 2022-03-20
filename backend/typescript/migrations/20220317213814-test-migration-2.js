d05e6047ca71e93e9ec9b24e17a032546038ef66
Mahad Zaryab <mahadzaryab1@gmail.com>
2022-03-20 10:32:53 -0400

    Revert To JS Migration

---
+++ backend/typescript/migrations/20220317213814-test-migration-2.js
@@ -0,0 +1,13 @@
+/* eslint-disable */
+"use strict";
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable("test", {
+      name: Sequelize.DataTypes.STRING,
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable("test");
+  },
+};
