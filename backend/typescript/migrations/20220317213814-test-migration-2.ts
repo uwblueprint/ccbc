9da7db96b2bcc5cd6f498039c4fa1fe4abb17764
Mahad Zaryab <mahadzaryab1@gmail.com>
2022-03-20 10:21:23 -0400

    Try Typescript Migration Template

--- backend/typescript/migrations/20220317213814-test-migration-2.ts
+++ backend/typescript/migrations/20220317213814-test-migration-2.ts
@@ -1,13 +1,28 @@
-/* eslint-disable */ 
-'use strict';
+// /* eslint-disable */ 
+// 'use strict';
+
+// module.exports = {
+//   up: (queryInterface, Sequelize) => {
+//     return queryInterface.createTable('test', {
+//       name: Sequelize.DataTypes.STRING,
+//     });
+//   },
+//   down: (queryInterface, Sequelize) => {
+//     return queryInterface.dropTable('test');
+//   }
+// };
+import { QueryInterface, DataTypes } from 'sequelize';
 
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('test', {
-      name: Sequelize.DataTypes.STRING,
-    });
-  },
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('test');
-  }
+    up: (queryInterface: QueryInterface): Promise<void> => {
+      return queryInterface.createTable("test", {
+        id: {
+          type: DataTypes.INTEGER
+        }
+      });
+    },
+
+    down: (queryInterface: QueryInterface): Promise<void> => {
+      return queryInterface.dropTable("test");
+    }
 };
