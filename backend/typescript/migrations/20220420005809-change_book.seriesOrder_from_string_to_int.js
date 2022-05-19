762a3bac92cc3b7c35ac853ef63168be0727a202
Jessie Peng <jessie.peng2010@gmail.com>
2022-05-18 22:00:07 -0400

    lint backend

--- backend/typescript/migrations/20220420005809-change_book.seriesOrder_from_string_to_int.js
+++ backend/typescript/migrations/20220420005809-change_book.seriesOrder_from_string_to_int.js
@@ -1,21 +1,19 @@
-'use strict';
-
 module.exports = {
-  async up (queryInterface, Sequelize) {
+  async up(queryInterface, Sequelize) {
     return Promise.all([
-      queryInterface.changeColumn('books', 'series_order', {
+      queryInterface.changeColumn("books", "series_order", {
         type: 'INTEGER USING CAST("series_order" as INTEGER)',
         // allowNull: true
-      })
-    ])
+      }),
+    ]);
   },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
     return Promise.all([
-      queryInterface.changeColumn('books', 'series_order', {
+      queryInterface.changeColumn("books", "series_order", {
         type: 'STRING USING CAST("series_order" as STRING)',
         // allowNull: true
-      })
-    ])
-  }
+      }),
+    ]);
+  },
 };
