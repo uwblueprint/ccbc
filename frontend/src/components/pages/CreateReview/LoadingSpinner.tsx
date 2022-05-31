59748c130e3d394af1841623cb59097252c886e9
Jessie Peng <jessie.peng2010@gmail.com>
2022-05-30 22:09:02 -0400

    added documentation

--- frontend/src/components/pages/CreateReview/LoadingSpinner.tsx
+++ frontend/src/components/pages/CreateReview/LoadingSpinner.tsx
@@ -1,6 +1,9 @@
 import { Center, Spinner } from "@chakra-ui/react";
 import React from "react";
 
+/**
+ * This component is the loading spinner that appears when data is being loaded onto the page
+ */
 const LoadingSpinner = (): React.ReactElement => {
   return (
     <Center mt="21%">
