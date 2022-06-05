3364141e08644318856cbb1d160e323b79ffdf4f
Nizar <nizarberserker@gmail.com>
2022-06-04 23:33:28 -0400

    test

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -19,7 +19,6 @@ import NotFound from "./components/pages/NotFound";
 import PreviewReviewTest from "./components/pages/PreviewReviewTest";
 import Profile from "./components/pages/Profile";
 import Unauthorized from "./components/pages/UnauthorizedPage";
-import UseToastHook from "./components/Toast";
 import { AUTHENTICATED_USER_KEY } from "./constants/AuthConstants";
 import { UserRole } from "./constants/Enums";
 import * as Routes from "./constants/Routes";
@@ -43,9 +42,8 @@ const App = (): React.ReactElement => {
     AUTHENTICATED_USER_KEY,
   );
 
-  const [authenticatedUser, setAuthenticatedUser] = useState<AuthenticatedUser>(
-    currentUser,
-  );
+  const [authenticatedUser, setAuthenticatedUser] =
+    useState<AuthenticatedUser>(currentUser);
 
   // Some sort of global state. Context API replaces redux.
   // Split related states into different contexts as necessary.
