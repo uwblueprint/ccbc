eac04d8f89e4953ccc4661c7a203a6565f3c10ab
christine <christineunlee@gmail.com>
2022-06-15 21:30:46 -0400

    Added migration file to create genres table and book_genres table

--- backend/typescript/migrations/20220612204742-create-genre-table.js
+++ backend/typescript/migrations/20220612204742-create-genre-table.js
@@ -3,24 +3,42 @@
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    return Promise.all([
-      queryInterface.createTable('genres', {
+    try {
+      await queryInterface.createTable('genres', {
         name: {
           type: Sequelize.DataTypes.STRING,
+          allowNull: false,
+          primaryKey: true,
+          unique: true,
+        }
+      });
+      await queryInterface.createTable('book_genre', {
+        genre_name: {
+          type: Sequelize.DataTypes.STRING,
+          allowNull: false,
+          references: {
+            model: 'genres',
+            key: 'name',
+          }
+        },
+        book_id: {
+          type: Sequelize.DataTypes.INTEGER,
           allowNull: false,
           references: {
             model: 'books',
             key: 'id',
-            as: 'book_id',
-          },
+          }
         }
-      }),
-    ]);
+      });
+      return Promise.resolve();
+    } catch (e) {
+      return Promise.reject(e);
+    }
   },
 
   async down(queryInterface, Sequelize) {
     return Promise.all([
-      queryInterface.dropTable("genres"),
+      queryInterface.dropTable("book_genre"),
     ]);
   },
 };
