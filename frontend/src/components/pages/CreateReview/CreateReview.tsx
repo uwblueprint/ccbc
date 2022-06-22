a98b36df0aa18638f21470cd53ca0ec9aee1c092
Nizar <nizarberserker@gmail.com>
2022-06-21 22:18:25 -0400

    Addressing pr comment

--- frontend/src/components/pages/CreateReview/CreateReview.tsx
+++ frontend/src/components/pages/CreateReview/CreateReview.tsx
@@ -64,13 +64,11 @@ const CreateReview = ({ id }: CreateReviewProps): React.ReactElement => {
   const [currBook, setCurrBook] = useState<Book | null>(null);
   const newToast = useToasts();
 
-  const [showDeleteBookModal, setShowDeleteBookModal] = useState<boolean>(
-    false,
-  );
+  const [showDeleteBookModal, setShowDeleteBookModal] =
+    useState<boolean>(false);
   const [showPublishModal, setShowPublishModal] = useState<boolean>(false);
-  const [showDeleteReviewModal, setShowDeleteReviewModal] = useState<boolean>(
-    false,
-  );
+  const [showDeleteReviewModal, setShowDeleteReviewModal] =
+    useState<boolean>(false);
   // const [showSaveDraftBeforeModal, setSaveDraftBeforeModal] =
   //   useState<boolean>(false);
   const [deleteBookIndex, setDeleteBookIndex] = useState<number>(-1);
@@ -168,9 +166,9 @@ const CreateReview = ({ id }: CreateReviewProps): React.ReactElement => {
   };
 
   /**
-   * Function to be called when the review is published.
+   * Function to be called when the review is published/saved as a draft.
    */
-  const onPublish = async (save = false) => {
+  const onPublishOrSave = async (save = false) => {
     // check if all fields have been filled in
     if (review !== "" || reviewerByline !== "" || books.length !== 0 || save) {
       // publish review
@@ -218,7 +216,7 @@ const CreateReview = ({ id }: CreateReviewProps): React.ReactElement => {
     setReviewError(false);
     setBylineError(false);
     if (review !== "" || reviewerByline !== "") {
-      onPublish(true);
+      onPublishOrSave(true);
     }
   };
 
@@ -304,7 +302,7 @@ const CreateReview = ({ id }: CreateReviewProps): React.ReactElement => {
       <PublishModal
         isOpen={showPublishModal}
         onClose={onPublishModalClose}
-        onPublish={onPublish}
+        onPublish={onPublishOrSave}
       />
       <DeleteReviewModal
         isOpen={showDeleteReviewModal}
@@ -317,7 +315,7 @@ const CreateReview = ({ id }: CreateReviewProps): React.ReactElement => {
           isOpen={showSaveDraftBeforeModal}
           onClose={onDeleteDraftReviewModalClose}
           deleteReview={() => {}}
-          saveReview={() => onPublish(true)}
+          saveReview={() => onPublishOrSave(save = true)}
           bookTitle="idk"
         />
       }
