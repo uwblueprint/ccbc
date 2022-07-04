be58b7c5870b094e8fde8511938e3a0b575a814b
Nizar <nizarberserker@gmail.com>
2022-07-03 22:01:28 -0400

    fixing linting issues

--- backend/typescript/rest/givecloud.ts
+++ backend/typescript/rest/givecloud.ts
@@ -7,26 +7,26 @@ import { Role } from "../types";
 const givecloudRouter: Router = Router();
 
 givecloudRouter.post("/", async (req, res) => {
-  const membership = req.body["membership"];
-  const supporter = req.body["supporter"];
-  let date = new Date();
+  const { membership } = req.body;
+  const { supporter } = req.body;
+  const date = new Date();
 
   const userService = new UserService();
   const authService = new AuthService(userService);
 
   try {
-    const email = supporter["email"];
+    const { email } = supporter;
     let roleType: Role = "Subscriber";
-    if (membership["name"] == "Professional Creator Membership") {
+    if (membership.name === "Professional Creator Membership") {
       roleType = "Author";
     }
     if ((await userService.getUserByEmail(email)) == null) {
-      date.setDate(date.getDate() + membership["days_to_expire"]);
+      date.setDate(date.getDate() + membership.days_to_expire);
       const user = await userService.createUser({
-        firstName: supporter["first_name"],
-        lastName: supporter["last_name"],
-        email: supporter["email"],
-        roleType: roleType,
+        firstName: supporter.first_name,
+        lastName: supporter.last_name,
+        email: supporter.email,
+        roleType,
         subscriptionExpiresOn: date,
         password: "",
       });
