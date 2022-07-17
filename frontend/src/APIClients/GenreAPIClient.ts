0afa730ececd3f9c48d41bb46d0aca20edc33706
christine <christineunlee@gmail.com>
2022-07-17 15:33:13 -0400

    Throw error instead of try/catch

--- frontend/src/APIClients/GenreAPIClient.ts
+++ frontend/src/APIClients/GenreAPIClient.ts
@@ -3,23 +3,23 @@ import { getBearerToken } from "../utils/AuthUtils";
 import baseAPIClient from "./BaseAPIClient";
 
 const getGenreOptions = async (): Promise<Option[]> => {
-  try {
-    const { data } = await baseAPIClient.get("/genres", {
-      headers: { Authorization: getBearerToken() },
-    });
+  const { data } = await baseAPIClient.get("/genres", {
+    headers: { Authorization: getBearerToken() },
+  });
 
-    const options: Option[] = [];
-    data.forEach((genre: Genre) => {
-      options.push({
-        value: genre.name,
-        label: genre.name,
-      });
-    });
-
-    return options;
-  } catch (error: unknown) {
-    return error as Option[];
+  if (!data) {
+    throw new Error(`Genres not found.`);
   }
+
+  const options: Option[] = [];
+  data.forEach((genre: Genre) => {
+    options.push({
+      value: genre.name,
+      label: genre.name,
+    });
+  });
+
+  return options;
 };
 
 export default { getGenreOptions };
