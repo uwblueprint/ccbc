9e4895cd5bc5e103a983eafccd77a578fd1e59bf
Nizar <nizarberserker@gmail.com>
2022-07-26 21:23:28 -0400

    logging req

--- backend/typescript/rest/givecloud.ts
+++ backend/typescript/rest/givecloud.ts
@@ -26,8 +26,9 @@ givecloudRouter.post(
   body("supporter.email", "suppoerter email is required").exists(),
   body("supporter.first_name", "supporter first_name is required").exists(),
   body("supporter.last_name", "supporter last_name is required").exists(),
-  async (req, res, next) => {
+  async (req, res) => {
     try {
+      console.log(req)
       const errors = validationResult(req);
       if (errors.isEmpty()) {
         res.status(400).json({ errors: errors.array() });
