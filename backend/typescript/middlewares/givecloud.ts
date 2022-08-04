1a5d84a33a632743f586a5dd6f79d6e498e87c6e
Nizar <nizarberserker@gmail.com>
2022-08-03 20:48:17 -0400

    updated doc string

--- backend/typescript/middlewares/givecloud.ts
+++ backend/typescript/middlewares/givecloud.ts
@@ -1,7 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 
 /* 
-checks the .env file to see if the givecloud webhook is enabled, 
+checks the environment variables to see if the givecloud webhook is enabled, 
 works if GIVECLOUD_WEBHOOK_ENABLE has a value of 1, the route is disabled if the value is anything else or not present
 */
 const isGiveCloudEnabled = () => {
