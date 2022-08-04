c93e5696de9948e67f74af306d7666ea7c294d2a
Nizar <nizarberserker@gmail.com>
2022-08-03 20:13:17 -0400

    fixing linting

--- backend/typescript/middlewares/validators/givecloudValidators.ts
+++ backend/typescript/middlewares/validators/givecloudValidators.ts
@@ -4,11 +4,17 @@ import { Request, Response, NextFunction } from "express";
 checks the .env file to see if the givecloud webhook is enabled, 
 works if GIVECLOUD_WEBHOOK_ENABLE has a value of 1, the route is disabled if the value is anything else or not present
 */
-export const isGiveCloudEnabled = () => {
-    return async(req: Request, res: Response, next: NextFunction): Promise<Response | void> =>{
-      if (process.env.GIVECLOUD_WEBHOOK_ENABLE !== "1") {
-        return res.status(404).json({ error: "Path does not exist" });
-      }
-      return next();
+const isGiveCloudEnabled = () => {
+  return async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ): Promise<Response | void> => {
+    if (process.env.GIVECLOUD_WEBHOOK_ENABLE !== "1") {
+      return res.status(404).json({ error: "Path does not exist" });
     }
+    return next();
   };
+};
+
+export default isGiveCloudEnabled
