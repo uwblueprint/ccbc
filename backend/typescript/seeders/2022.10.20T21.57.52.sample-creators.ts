d9e923272f11deb4d0acba3a4249430363b9f72d
Anish Aggarwal <anish.5@hotmail.com>
2022-10-24 17:59:02 -0400

    Creator Model Setup (#183)
    * Creator Model Setup
    
    * Rename file

---
+++ backend/typescript/seeders/2022.10.20T21.57.52.sample-creators.ts
@@ -0,0 +1,41 @@
+import { Seeder } from "../umzug";
+
+const seedCreators = [
+  {
+    id: 1,
+    user_id: 1,
+    location: "Canada",
+    rate: 4.6,
+    genre: "Mystery",
+    age_group: "Teenager",
+    timezone: "EDT",
+    bio: "A mysterious author who writes detective stories to unravel great conspiracies",
+    is_approved: true,
+    createdAt: new Date(Date.now()),
+    updatedAt: new Date(Date.now()),
+  },
+  {
+    id: 2,
+    user_id: 1,
+    location: "USA",
+    rate: 2.3,
+    genre: "Horror",
+    age_group: "Children",
+    timezone: "EDT",
+    bio: "A proactive author using books to teach youngsters about the horrors in the real world like those found in abandoned ghost towns",
+    is_approved: false,
+    createdAt: new Date(Date.now()),
+    updatedAt: new Date(Date.now()),
+  },
+];
+
+export const up: Seeder = async ({ context: sequelize }) => {
+  await sequelize.query("TRUNCATE TABLE creator CASCADE");
+  await sequelize.getQueryInterface().bulkInsert("creator", seedCreators);
+};
+
+export const down: Seeder = async ({ context: sequelize }) => {
+  await sequelize
+    .getQueryInterface()
+    .bulkDelete("creator", { id: seedCreators.map((u) => u.id) });
+};
