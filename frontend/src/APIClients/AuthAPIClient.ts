5720c8381447a8fa91b0fa3fb8f66645b9f854f1
Shrinjay <shrinjaymukherjee@gmail.com>
2022-11-24 19:13:06 -0500

    open up api client

--- frontend/src/APIClients/AuthAPIClient.ts
+++ frontend/src/APIClients/AuthAPIClient.ts
@@ -62,15 +62,15 @@ const register = async (
   lastName: string,
   email: string,
 ): Promise<AuthenticatedUser> => {
-  const bearerToken = `Bearer ${getLocalStorageObjProperty(
-    AUTHENTICATED_USER_KEY,
-    "accessToken",
-  )}`;
+  // const bearerToken = `Bearer ${getLocalStorageObjProperty(
+  //   AUTHENTICATED_USER_KEY,
+  //   "accessToken",
+  // )}`;
   try {
     const { data } = await baseAPIClient.post(
       "/auth/register",
       { firstName, lastName, email },
-      { withCredentials: true, headers: { Authorization: bearerToken }},
+      { withCredentials: true},
     );
     return data;
   } catch (error) {
