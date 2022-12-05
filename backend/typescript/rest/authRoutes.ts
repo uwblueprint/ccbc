7caf7d64679d298a84eb18de8eeeac466c556cf6
Calder White <calderwhite1@gmail.com>
2022-12-05 16:31:11 -0500

    Follow up lint fix :))))

--- backend/typescript/rest/authRoutes.ts
+++ backend/typescript/rest/authRoutes.ts
@@ -1,9 +1,6 @@
 import { Router } from "express";
 
-import {
-  isAuthorizedByEmail,
-  isAuthorizedByUserId,
-} from "../middlewares/auth";
+import { isAuthorizedByEmail, isAuthorizedByUserId } from "../middlewares/auth";
 import {
   loginRequestValidator,
   registerRequestValidator,
