40a76b79686fdaffbb593f37caf38fed04050a3a
Derrick Yang <yangguangzheng122@gmail.com>
2023-01-29 19:30:55 -0500

    lint errors

--- backend/typescript/rest/creatorBookingRoutes.ts
+++ backend/typescript/rest/creatorBookingRoutes.ts
@@ -11,33 +11,30 @@ const creatorBookingRouter: Router = Router();
 const creatorBookingService: ICreatorBookingService =
   new CreatorBookingService();
 
-creatorBookingRouter.post(
-  "/",
-  async (req, res) => {
-    const contentType = req.headers["content-type"];
-    try {
-      const newBooking = await creatorBookingService.addCreatorBooking({
-        creatorId: req.body.creatorId,
-        name: req.body.name,
-        email: req.body.email,
-        date: req.body.date,
-        isTentative: req.body.isTentative,
-        isOneDay: req.body.isOneDay,
-        ageGroup: req.body.ageGroup,
-        audienceSize: req.body.audienceSize,
-        subject: req.body.subject,
-        message: req.body.message,
-      });
-      await sendResponseByMimeType<CreatorBookingResponseDTO>(
-        res,
-        200,
-        contentType,
-        newBooking,
-      );
-    } catch (e: unknown) {
-      sendErrorResponse(e, res);
-    }
-  },
-);
+creatorBookingRouter.post("/", async (req, res) => {
+  const contentType = req.headers["content-type"];
+  try {
+    const newBooking = await creatorBookingService.addCreatorBooking({
+      creatorId: req.body.creatorId,
+      name: req.body.name,
+      email: req.body.email,
+      date: req.body.date,
+      isTentative: req.body.isTentative,
+      isOneDay: req.body.isOneDay,
+      ageGroup: req.body.ageGroup,
+      audienceSize: req.body.audienceSize,
+      subject: req.body.subject,
+      message: req.body.message,
+    });
+    await sendResponseByMimeType<CreatorBookingResponseDTO>(
+      res,
+      200,
+      contentType,
+      newBooking,
+    );
+  } catch (e: unknown) {
+    sendErrorResponse(e, res);
+  }
+});
 
 export default creatorBookingRouter;
