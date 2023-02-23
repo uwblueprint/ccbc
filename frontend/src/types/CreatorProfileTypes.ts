e107a6d0abc4c8a13a38fe1bbdd5aa3950613798
Caitlin Kwan <29318811+caitlinkwan@users.noreply.github.com>
2023-02-22 20:59:16 -0500

    Creator Profile Publications Form (#215)
    * Implement all frontend features
    
    * Set up book cover upload functionality, refactoring
    
    * Add error handling
    
    * Fix state initial value
    
    * Add yarn file
    
    ---------
    
    Co-authored-by: Caitlin Kwan <caitlinkwan@Caitlins-MacBook-Air.local>

--- frontend/src/types/CreatorProfileTypes.ts
+++ frontend/src/types/CreatorProfileTypes.ts
@@ -7,6 +7,8 @@ export type CreatorProfile = {
   city?: string;
   province?: string;
   postalCode?: string;
+  bibliography?: BibliographyEntry[];
+  bookCovers?: BookCoverFile[];
   geographicReach?: string;
   primaryTimezone?: string;
   availability?: string[];
@@ -23,3 +25,16 @@ export type CreatorProfileProps =
   | "postalCode"
   | "geographicReach"
   | "primaryTimezone";
+
+export type BibliographyEntry = {
+  title: string;
+  publisher: string;
+  publicationYear: number;
+  additionalNotes?: string;
+};
+
+export type BookCoverFile = {
+  url: string;
+  name: string;
+  fileSize: number;
+};
