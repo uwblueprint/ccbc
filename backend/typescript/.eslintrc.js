d3aba6bd490444161cf2f949e10b3a448f367fb4
Shrinjay Mukherjee <shrinjaymukherjee@gmail.com>
2023-02-22 20:27:10 -0700

    lint and seeder fix

--- backend/typescript/.eslintrc.js
+++ backend/typescript/.eslintrc.js
@@ -15,6 +15,7 @@ module.exports = {
     "plugin:@typescript-eslint/eslint-recommended",
     "plugin:@typescript-eslint/recommended",
   ],
+  ignorePatterns: ["migrations/**", "build/**"],
   rules: {
     "prettier/prettier": ["error", { endOfLine: "auto" }],
   },
