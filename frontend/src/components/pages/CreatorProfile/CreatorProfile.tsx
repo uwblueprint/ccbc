59533de5ba5b55f16a1111ebe7aa2a11972cd2e5
Sebastian Mendoza <sebmendozaa@gmail.com>
2023-03-05 21:07:50 -0500

    created overview component and changed background

--- frontend/src/components/pages/CreatorProfile/CreatorProfile.tsx
+++ frontend/src/components/pages/CreatorProfile/CreatorProfile.tsx
@@ -1,23 +1,13 @@
 import { ArrowBackIcon, EmailIcon } from "@chakra-ui/icons";
-import {
-  Box,
-  Button,
-  Center,
-  Flex,
-  Grid,
-  GridItem,
-  Heading,
-  Image,
-  Link as ChakraLink,
-  Text,
-  VStack,
-} from "@chakra-ui/react";
+import { Box, Button, Center, Flex, Heading, Text } from "@chakra-ui/react";
 import React, { useEffect, useState } from "react";
 import { Link, useHistory, useParams } from "react-router-dom";
 
 import CreatorAPIClient from "../../../APIClients/CreatorAPIClient";
+import background from "../../../assets/SearchResultsBackground.png";
 import { Creator } from "../../../types/CreatorTypes";
 import LoadingSpinner from "../../common/LoadingSpinner";
+import CreatorOverview from "./CreatorOverview";
 
 interface CreatorProfileParams {
   id: string;
@@ -53,75 +43,39 @@ const CreatorProfile = (): React.ReactElement => {
   return currentCreator ? (
     <div>
       <>
-        <Center mt="30px">
-          <Box w="80%">
-            <Flex align="center" mb="40px" gap="8px">
-              <ArrowBackIcon boxSize={6} />
-              <Link to="..">
-                <Text fontSize="lg" color="#000" fontWeight="semibold">
-                  Return to Creator Directory
-                </Text>
-              </Link>
-            </Flex>
-
-            <Flex gap="31px" alignItems="center">
-              <Heading as="h2" size="xl">
-                {currentCreator.firstName} {currentCreator.lastName}
-              </Heading>
-              <Button leftIcon={<EmailIcon />} variant="add" cursor="pointer">
-                Contact
-              </Button>
-            </Flex>
-
-            <Box boxShadow="lg" padding="5" my="20px">
-              <Flex
-                flexDir={{ base: "column", md: "row" }}
-                gap={{ base: "20px", md: "50px" }}
-              >
-                <Image
-                  borderRadius="sm"
-                  maxW="100%"
-                  maxH="349px"
-                  // src="https://bit.ly/dan-abramov"
-                  src="https://storage.googleapis.com/ccbc-95e66.appspot.com/images/86b092bb-b082-49d9-ac32-790f0a1472b8"
-                  objectFit="cover"
-                />
-                <Grid gridTemplateColumns="120px 1fr">
-                  <GridItem fontWeight="bold">
-                    <VStack gap={2} py="10px" align="start">
-                      <div> Craft</div>
-                      <div> Genre</div>
-                      <div> Audience</div>
-                      <div> Province</div>
-                      <div> Timezone</div>
-                      <div> Website</div>
-                      <div> Bio</div>
-                    </VStack>
-                  </GridItem>
-                  <GridItem w="100%">
-                    <VStack gap={2} py="10px" align="start">
-                      <Box py="2px" px="8px" bg="green.100" borderRadius="sm">
-                        {currentCreator.craft}
-                      </Box>
-                      <div> {currentCreator.genre?.replace(/[{}]/g, "")}</div>
-                      <div>
-                        {currentCreator.ageRange
-                          ?.replace(/[^0-9\s]/g, "")
-                          .replace(" ", "-")}
-                      </div>
-                      <div> {currentCreator.province}</div>
-                      <div> {currentCreator.timezone}</div>
-                      <ChakraLink href="www.google.com">
-                        {currentCreator.website}
-                      </ChakraLink>
-                      <div> {currentCreator.bio}</div>
-                    </VStack>
-                  </GridItem>
-                </Grid>
+        <Box
+          minW="100vw"
+          minH="100vh"
+          bgImage={[null, null, background]}
+          bgRepeat="no-repeat"
+          backgroundSize="cover"
+          backgroundAttachment="scroll"
+          bgPosition="0 -120px"
+        >
+          <Center>
+            <Box w="80%">
+              <Flex align="center" mt="20px" mb="40px" gap="8px">
+                <ArrowBackIcon boxSize={6} />
+                <Link to="..">
+                  <Text fontSize="lg" color="#000" fontWeight="semibold">
+                    Return to Creator Directory
+                  </Text>
+                </Link>
               </Flex>
+
+              <Flex gap="31px" alignItems="center">
+                <Heading as="h2" size="xl">
+                  {currentCreator.firstName} {currentCreator.lastName}
+                </Heading>
+                <Button leftIcon={<EmailIcon />} variant="add" cursor="pointer">
+                  Contact
+                </Button>
+              </Flex>
+
+              <CreatorOverview currentCreator={currentCreator} />
             </Box>
-          </Box>
-        </Center>
+          </Center>
+        </Box>
       </>
     </div>
   ) : (
