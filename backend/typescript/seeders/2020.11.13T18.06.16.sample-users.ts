b4a899bedbc25220f09ba9972fcfe8ad0878e961
Shrinjay Mukherjee <shrinjaymukherjee@gmail.com>
2023-03-08 18:49:43 -0700

    add user seeder

---
+++ backend/typescript/seeders/2020.11.13T18.06.16.sample-users.ts
@@ -0,0 +1,24 @@
+import { Seeder } from "../umzug";
+
+const seedUsers = [
+  {
+    id: 1,
+    auth_id: "fasdfsad",
+    role_type: "Admin",
+    first_name: "Shrinjay",
+    last_name: "Mukherjee",
+    email: "shrinjaymukherjee@gmail.com",
+    createdAt: new Date(1636226732806),
+    updatedAt: new Date(1636226732806),
+  }
+];
+
+export const up: Seeder = async ({ context: sequelize }) => {
+  await sequelize.query("TRUNCATE TABLE users CASCADE");
+  await sequelize.getQueryInterface().bulkInsert("users", seedUsers);
+};
+export const down: Seeder = async ({ context: sequelize }) => {
+  await sequelize
+    .getQueryInterface()
+    .bulkDelete("users", { id: seedUsers.map((u) => u.id) });
+};
