41dfc85031cd586c90fe330b8b007a911f4d7a01
Derrick Yang <yangguangzheng122@gmail.com>
2023-03-19 20:02:19 -0400

    bottom legend for availability grid

--- frontend/src/components/pages/CreatorProfile/CreatorAvailability.tsx
+++ frontend/src/components/pages/CreatorProfile/CreatorAvailability.tsx
@@ -1,6 +1,6 @@
 
 
-import { Flex, Grid, GridItem, Text } from "@chakra-ui/react";
+import { Button, Flex, Grid, GridItem, Text } from "@chakra-ui/react";
 import React from "react";
 
 import { Creator } from "../../../types/CreatorTypes";
@@ -57,6 +57,10 @@ const CreatorAvailability = ({
     );
   };
 
+  const timezone = currentCreator?.timezone
+  const d = new Date()
+  let diff = d.getTimezoneOffset()
+
   return (
     <Flex flex="1" direction="column" justify="start">
       <Text textStyle="h2" textAlign="left" fontWeight="bold">
@@ -66,7 +70,7 @@ const CreatorAvailability = ({
         This is to help you get a sense of scheduling. Please contact the creator to inquire about details.
       </Text>
       <Grid
-        templateRows="repeat(3, 1fr)"
+        templateRows="repeat(4, 1fr)"
         templateColumns="repeat(8, 1fr)"
         gap="4px"
         width="100%"
@@ -87,7 +91,47 @@ const CreatorAvailability = ({
         {slots.slice(7).map((slot) => {
           return createAvailabilitySlot(slot);
         })}
+        <GridItem />
+        <GridItem 
+          textAlign="left"
+          colSpan={2}
+        >
+        <Button 
+          bg="white"
+          size="xs"
+          minWidth="0"
+          height="15px"
+          width="15px"
+          borderRadius="4px" 
+        />
+        <Text
+          as="span"
+          paddingRight="20px"
+        >
+          {" Busy"}
+        </Text> 
+        <Button 
+          bg='#C4F1F9'
+          size="xs"
+          minWidth="0"
+          height="15px"
+          width="15px"
+          borderRadius="4px" 
+        />
+          <Text
+          as="span"
+        >
+          {" Available"}
+        </Text> 
+        </GridItem>
+        <GridItem
+          textAlign="right"
+          colSpan={5}
+        >
+          Creator's Timezone: {timezone ?? ""}
+        </GridItem>
       </Grid>
+     
       
     </Flex>
     //
