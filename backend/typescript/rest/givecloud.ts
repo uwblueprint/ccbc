80f5eb90d9957fe272e0431c6a59c7e186614717
Derrick Yang <yangguangzheng122@gmail.com>
2023-04-26 21:22:21 -0400

    WIP

--- backend/typescript/rest/givecloud.ts
+++ backend/typescript/rest/givecloud.ts
@@ -34,18 +34,18 @@ givecloudRouter.post(
   body("supporter.last_name", "supporter last_name is required").exists(),
   async (req: Request, res: Response) => {
     try {
-      if (process.env.HMAC_SECRET_KEY) {
-        const hash = createHmac("SHA1", process.env.HMAC_SECRET_KEY)
-          .update(JSON.stringify(req.body))
-          .digest("hex");
+      // if (process.env.HMAC_SECRET_KEY) {
+      //   const hash = createHmac("SHA1", process.env.HMAC_SECRET_KEY)
+      //     .update(JSON.stringify(req.body))
+      //     .digest("hex");
 
-        if (hash !== req.get("X-Givecloud-Signature")) {
-          res.status(401).send("Unauthorized");
-          return;
-        }
-      } else {
-        throw new Error("No HMAC secret key set");
-      }
+      //   if (hash !== req.get("X-Givecloud-Signature")) {
+      //     res.status(401).send("Unauthorized");
+      //     return;
+      //   }
+      // } else {
+      //   throw new Error("No HMAC secret key set");
+      // }
       const errors = validationResult(req);
       if (errors.isEmpty()) {
         const { supporter } = req.body;
